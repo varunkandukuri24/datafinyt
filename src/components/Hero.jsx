ee761ad87de019ecc82bf26e3a557b64356ab445
varunkandukuri24 <vkandukuri24@gmail.com>
2023-10-14 22:27:48 -0700

    finished!

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
             <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6'>Grow with data</h1>
             <div className='flex justify-center items-center'>
                 <p className='md:text-5xl sm:text-4xl text-xl font-bold py-4'>Fast, Flexible, Financing for</p>
-                <Typed className='md:text-5xl sm:text-4xl text-xl font-bold md: pl-4 pl-2' strings={[
+                <Typed className='md:text-5xl sm:text-4xl text-xl font-bold md: pl-2' strings={[
                     'B2B',
                     'B2C',
                     'SAAS']} typeSpeed={120} backSpeed={140} loop/>
